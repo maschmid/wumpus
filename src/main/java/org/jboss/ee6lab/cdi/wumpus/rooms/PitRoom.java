da7c5ca5578af180a054bbe0c8d3f3a20a8f20d2
Marek Schmidt <maschmid@redhat.com>
2012-12-04 10:59:52 +0100

    make rooms application scoped

--- src/main/java/org/jboss/ee6lab/cdi/wumpus/rooms/PitRoom.java
+++ src/main/java/org/jboss/ee6lab/cdi/wumpus/rooms/PitRoom.java
@@ -1,13 +1,14 @@
 package org.jboss.ee6lab.cdi.wumpus.rooms;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 
 import org.jboss.ee6lab.cdi.wumpus.xmlbeans.Room;
 import org.jboss.ee6lab.cdi.wumpus.xmlbeans.RoomName;
 
 @RoomName("pit")
-@ApplicationScoped
+@SessionScoped
 public class PitRoom extends Room {
 	
 	@Inject
--- src/main/java/org/jboss/ee6lab/cdi/wumpus/rooms/SouthRoom.java
+++ src/main/java/org/jboss/ee6lab/cdi/wumpus/rooms/SouthRoom.java
@@ -1,13 +1,14 @@
 package org.jboss.ee6lab.cdi.wumpus.rooms;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 
 import org.jboss.ee6lab.cdi.wumpus.xmlbeans.Room;
 import org.jboss.ee6lab.cdi.wumpus.xmlbeans.RoomName;
 
 @RoomName("south")
-@ApplicationScoped
+@SessionScoped
 public class SouthRoom extends Room {
 	@Inject
 	public void inject(@RoomName("pit") Room pitRoom, @RoomName("entry") Room entryRoom) {
